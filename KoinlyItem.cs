adb2e4c0abdab52b34b581c853766a04abbd81fe
rhenium2 <5300091+rhenium2@users.noreply.github.com>
2023-02-06 17:40:29 +1100

    first files added

---
+++ KoinlyItem.cs
@@ -0,0 +1,20 @@
+using CsvHelper.Configuration.Attributes;
+
+namespace FluxToKoinly;
+
+public class KoinlyItem
+{
+    [Name("Date")] public string Date { get; set; }
+
+    [Name("Sent Amount")] public double SentAmount { get; set; }
+
+    [Name("Sent Currency")] public string SentCurrency { get; set; }
+
+    [Name("Received Amount")] public double ReceivedAmount { get; set; }
+
+    [Name("Received Currency")] public string ReceivedCurrency { get; set; }
+
+    [Name("Label")] public string Label { get; set; }
+    [Name("Description")] public string Description { get; set; }
+    [Name("TxHash")] public string TxHash { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using System.Reflection;
+using CsvHelper;
+using FluxApi.SDK;
+using FluxToKoinly;
+
+var accountId = args[0];
+
+Console.WriteLine($"Flux to Koinly v{Assembly.GetExecutingAssembly().GetName().Version.ToString()}");
+var transactions = await FluxApiService.GetAccountRawTransactions(accountId);
+
+await using var writer = new StreamWriter($"{accountId}_transactions.csv");
+await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+csv.WriteHeader<KoinlyItem>();
+csv.NextRecord();
+foreach (var transaction in transactions)
+{
+    var koinlyItem = new KoinlyItem
+    {
+        Date = UnixTimeStampToDateTime(transaction.Blocktime).ToString("yyyy-MM-dd HH:mm:ss"),
+        TxHash = transaction.Blockhash
+    };
+
+    var isReceived = transaction.Vout.Any(x => x.ScriptPubKey.Addresses.Any(a => a == accountId));
+    var vouts = transaction.Vout.FirstOrDefault(x => x.ScriptPubKey.Addresses.Any(a => a == accountId));
+    if (vouts is not null)
+    {
+        koinlyItem.ReceivedAmount = vouts.Value;
+        koinlyItem.ReceivedCurrency = "FLUX";
+        koinlyItem.Label = "reward";
+        koinlyItem.Description = "reward";
+    }
+
+    csv.WriteRecord(koinlyItem);
+    csv.NextRecord();
+}
+
+
+static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+{
+    // Unix timestamp is seconds past epoch
+    var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+    dateTime = dateTime.AddSeconds(unixTimeStamp);
+    return dateTime;
+}
